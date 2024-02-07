c0ec95a7c22595c02373d23fd45a56f3ffa470e0
Lastraum <lastraumonline@gmail.com>
2024-02-06 18:38:54 -0800

    ui && functions
    **ui**
    - changed ui atlas
    - added teleport component
    
    **functions**
    - added teleport action component
    - added template for custom code

--- src/iwb/components/custom/index.ts
+++ src/iwb/components/custom/index.ts
@@ -1,9 +1,6 @@
 
-import { createBuildCompetitionSignup } from "./competitions/BuildCompetition";
 import { createCustomUI } from "./ui";
-
 
 export function createCustomCode(room:any){
     createCustomUI()
-    createBuildCompetitionSignup(room)
 }
