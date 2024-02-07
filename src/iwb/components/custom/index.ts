8146fbec185556a3b45cb4d2346bafe6e80b1918
Lastraum <lastraumonline@gmail.com>
2024-02-06 22:02:10 -0800

    Update index.ts

--- src/iwb/components/custom/index.ts
+++ src/iwb/components/custom/index.ts
@@ -1,6 +1,8 @@
 
+import { createBuildCompetitionSignup } from "./competitions/BuildCompetition";
 import { createCustomUI } from "./ui";
 
 export function createCustomCode(room:any){
     createCustomUI()
+    createBuildCompetitionSignup(room)
 }
