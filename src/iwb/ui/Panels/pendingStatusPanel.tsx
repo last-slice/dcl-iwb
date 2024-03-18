f80280177ab544c4d5771e710dc2636f834050e3
Lastraum <lastraumonline@gmail.com>
2024-03-18 16:46:43 -0700

    Update pendingStatusPanel.tsx

--- src/iwb/ui/Panels/pendingStatusPanel.tsx
+++ src/iwb/ui/Panels/pendingStatusPanel.tsx
@@ -46,7 +46,7 @@ export function createPendingStatusPanel(){
       alignContent:'center',
       alignItems:'center',
       positionType:'absolute',
-      position:{top: '1%', left: isPreview ? '13%' : '11%'}
+      position:{top: '1%', left: isPreview ? '13%' : '13%'}
     }}
     uiBackground={{
       texture:{
